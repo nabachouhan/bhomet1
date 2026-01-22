e1c73212f469b749325ad6ce1a7e751ace1d76b1
nabachouhan <nabakkrr@gmail.com>
2026-01-22 13:26:55 +0530

    first commit

---
+++ src/db/connection.js
@@ -0,0 +1,46 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const poolUser = new Pool({
+    user: process.env.db_user,
+    host: process.env.db_host,
+    database:process.env.db_for_user,
+    password: process.env.db_pw,
+    port: process.env.db_port
+});
+
+
+// 👉 Theme-based pool map
+const themeDatabases = {
+  administrative: process.env.Administrative_db,
+  aoi: process.env.aoi_db,
+  lulc: process.env.lulc_db,
+  lulcchange: process.env.lulcchange_db,
+  densechange: process.env.densechange_db ,
+};
+
+const poolMap = {};
+for (const [theme, dbName] of Object.entries(themeDatabases)) {
+  poolMap[theme] = new Pool({
+    user: process.env.db_user,
+    host: process.env.host,
+    database: dbName,
+    password: process.env.db_pw,
+    port: process.env.db_port
+  });
+}
+
+// 👉 Function to get theme-based pool
+function getPoolByTheme(theme) {
+  const pool = poolMap[theme.toLowerCase()];
+  if (!pool) throw new Error(`No theme database pool found for: ${theme}`);
+  return pool;
+}
+
+
+export { poolUser,  getPoolByTheme };
+
+
+console.log("connected")
